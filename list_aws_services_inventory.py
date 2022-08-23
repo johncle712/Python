3523572d7dc4d60263d85028461e8ef8106fd1e8
John Le <johncle712@gmail.com>
2022-08-23 15:05:02 +0000

    added append command to add AWS services to list

--- list_aws_services_inventory.py
+++ list_aws_services_inventory.py
@@ -6,3 +6,12 @@ aws_list = []
 print("\nThis prints an empty list:")
 print(aws_list)
 
+#Use the append command to add multiple elements to a list
+print("\nThis adds AWS services to our list:")
+aws_list.append("S3")
+aws_list.append("Lambda")
+aws_list.append("EC2")
+aws_list.append("SNS")
+aws_list.append("SQS")
+aws_list.append("DynamoDB")
+print(aws_list)
