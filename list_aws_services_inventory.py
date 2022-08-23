44514eb2f82cc8d6267904fbdd8fb850c9188d57
John Le <johncle712@gmail.com>
2022-08-23 14:55:45 +0000

    Create aws_list varible to print out empty list

---
+++ list_aws_services_inventory.py
@@ -0,0 +1,8 @@
+#For this project. This script will list out various AWS services.
+#We will populate an empty list, add to the list, print the list length, remove services and print a new list.
+
+#Define and print out an empty list
+aws_list = []
+print("\nThis prints an empty list:")
+print(aws_list)
+
