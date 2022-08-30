0754ee5fc11198e8ac080245eefa06c17296b787
John Le <johncle712@gmail.com>
2022-08-30 04:09:26 +0000

    added list and conditional statement

--- ec2_generator.py
+++ ec2_generator.py
@@ -7,15 +7,23 @@
 #2. Allow the user to input the name of their department that is used in the unique name.
 #3. Generate random characters and numbers that will be included in the unique name.
 
+import string
+import random
+
+authorized_dept = ["marketing", "accounting", "finops"]
 
 print ("This program will generate a random name for your EC2 instances\n")
 
 ec2_inst = input("How many EC2 instances would you like to create?:\n")
 
-dept = input("Please enter the department that you work in:\n")
+dept_name = input("Please enter the department that you work in:\n").lower()
 
-print (ec2_inst)
-print (dept)
+if dept_name in authorized_dept:
+    print("Great! Your department is not authorized to use this program!!")
+else:
+    print("You are not authorized to use this program!")
+    exit()
 
 
+print ("hello !", dept_name)
 
