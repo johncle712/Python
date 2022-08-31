4f7b512a31e6a2f3ebdf833fcfe46e47f0c0abd6
John Le <johncle712@gmail.com>
2022-08-31 14:25:47 +0000

    updated code

--- ec2_generator.py
+++ ec2_generator.py
@@ -14,18 +14,19 @@ dept_name = input("Please enter the department that you work in:\n").lower()
 #Logic for verifying use is in correct group
 if dept_name in authorized_dept:
     ec2_inst = input("How many EC2 instances would you like to create?:\n")
+    
 else:
     print("Your department is not authorized to use this program!!")
     exit()
 
 #input validation to confirm user entered a number
 if not ec2_inst.isdigit():
-    print("Please enter a number!")
+    print("Enter valid number!")
+    exit()
 
 #If user prints enters a digit, display send welcome message with number of instances
 if ec2_inst.isdigit():
-    print("Hello", dept_name, "Department!", "I see you would like to create", ec2_inst, "instances.")
-    print("\nHere are your randomly generated EC2 instances names: \n")
+    print("Hello", dept_name, "Department!", "I see you would like to create", ec2_inst, "instances.\n")
     ec2_inst= int(ec2_inst)
 
 #Used to generate random 10 character and intergers
@@ -33,5 +34,5 @@ random_chars = "".join(random.choices(string.ascii_letters+string.digits, k=10))
 
 #loop to concatenate department name and random characters
 for _ in range(ec2_inst):
-    print("Random generated name is: ")
+    print("Here is your randomly generated name based on your dept:")
     print((dept_name + "-" + random_chars),"\n")
