f23255c4e9f03b66164208b95a818a942c617f0f
John Le <johncle712@gmail.com>
2022-09-04 16:21:48 -0500

    corrected character casing for s3 and bucket

--- S3/S3_create_bucket.py
+++ S3/S3_create_bucket.py
@@ -1,8 +1,7 @@
 import boto3
 
-aws_resource = boto3.resource ("S3")
-bucket = aws_resource.bucket ("boto3_LUIT")
-
+aws_resource = boto3.resource ("s3")
+bucket = aws_resource.Bucket ("boto3_LUIT")
 response = bucket.create(
     ACL= 'public-read',
     CreateBucketConfiguration={
