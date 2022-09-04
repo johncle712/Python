64b959776359b834b9f1c49fe802590da78759a8
John Le <johncle712@gmail.com>
2022-09-04 16:30:44 -0500

    removed location constraint

--- S3/S3_create_bucket.py
+++ S3/S3_create_bucket.py
@@ -1,12 +1,10 @@
 import boto3
 
-aws_resource = boto3.resource ("s3")
-bucket = aws_resource.Bucket ("boto3_LUIT")
+aws_resource = boto3.resource("s3")
+bucket = aws_resource.Bucket("boto3-LUIT-jl7392384")
+
 response = bucket.create(
     ACL= 'public-read',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-1'
-    },
 )
 
 print(response)
