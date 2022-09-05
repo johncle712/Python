3f1cd9d939a89808e949e81a34b313ccdefbb980
John Le <johncle712@gmail.com>
2022-09-05 00:32:33 +0000

    updated bucketname

--- S3/S3_create_bucket.py
+++ S3/S3_create_bucket.py
@@ -1,7 +1,7 @@
 import boto3
 
-aws_resource = boto3.resource("s3")
-bucket = aws_resource.Bucket("boto3-LUIT-jl7392384")
+aws_resource=boto3.resource("s3")
+bucket=aws_resource.Bucket("boto3-luit-jl7392384")
 
 response = bucket.create(
     ACL= 'public-read',
