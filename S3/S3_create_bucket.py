4525ffb63f87c12967c0b0b87c22470b6cb29d22
John Le <johncle712@gmail.com>
2022-09-05 11:51:35 -0500

    changed access to private

--- S3/S3_create_bucket.py
+++ S3/S3_create_bucket.py
@@ -4,7 +4,7 @@ aws_resource=boto3.resource("s3")
 bucket=aws_resource.Bucket("boto3-luit-jl7392384")
 
 response = bucket.create(
-    ACL= 'public-read',
+    ACL= 'private',
 )
 
 print(response)
---
+++ S3/s3_upload_file.py
@@ -0,0 +1,12 @@
+import resource
+import boto3
+
+s3=boto3.client('s3')
+
+#upload single file
+s3.upload_file(
+    Filename="/Users/johnle/Desktop/test.jpg",
+    Bucket="boto3-luit-jl7392384",
+    Key="test.jpg"
+)
+
